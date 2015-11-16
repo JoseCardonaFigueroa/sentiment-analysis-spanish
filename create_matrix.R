4e220828d9f1bbcdf93acac570ea4c2078f8e5da
José Cardona Figueroa <cardona5d@gmail.com>
2015-11-16 12:15:49 -0600

    Agregue el archivo parametros a termDocumentMatrix para que no ignorara las palabras menores a tres de longitud

--- create_matrix.R
+++ create_matrix.R
@@ -1,13 +1,15 @@
-create_matrix <- function(textColumns, language="spanish", minDocFreq=1, minWordLength=0, removeNumbers=TRUE, removePunctuation=TRUE, removeSparseTerms=0, removeStopwords=FALSE, stemWords=FALSE, stripWhitespace=TRUE, toLower=TRUE, weighting=weightTf) {
+create_matrix <- function(textColumns, language="spanish", minDocFreq=1, minWordLength=1, maxWordLength=100, removeNumbers=TRUE, removePunctuation=TRUE, removeSparseTerms=0, removeStopwords=FALSE, stemWords=FALSE, stripWhitespace=TRUE, toLower=TRUE, weighting=weightTf) {
 	
     stem_words <- function(x) {
         split <- strsplit(x," ")
-        return(wordStem(split[[1]],language=language))
+        return(split[[1]],language=language)
     }
 	
-	control <- list(language=language,tolower=toLower,removeNumbers=removeNumbers,removePunctuation=removePunctuation,stripWhitespace=stripWhitespace,minWordLength=minWordLength,stopwords=removeStopwords,minDocFreq=minDocFreq,weighting=weighting)
+	control <- list(language=language,tolower=toLower,removeNumbers=removeNumbers,removePunctuation=removePunctuation,stripWhitespace=stripWhitespace,wordLengths=c(minWordLength,maxWordLength),stopwords=removeStopwords,minDocFreq=minDocFreq,weighting=weighting)
     
-    if (stemWords == TRUE) control <- append(control,list(stemming=stem_words),after=6)
+    if (stemWords == TRUE){
+      control <- append(control,list(stemming=stem_words),after=6)
+    }
     
     trainingColumn <- apply(as.matrix(textColumns),1,paste,collapse=" ")
     trainingColumn <- sapply(as.vector(trainingColumn,mode="character"),iconv,to="UTF8",sub="byte")
